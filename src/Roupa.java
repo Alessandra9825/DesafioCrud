e3842fd35cc426add01064b56eae2d7dc7bdea09
Lucas Araujo Russo <lukas_russo_92@hotmail.com>
2020-09-02 21:00:37 -0300

    MInha classe

---
+++ src/Roupa.java
@@ -0,0 +1,58 @@
+import java.util.Date;
+
+public class Roupa {
+    private int codigoItem;
+    private Date dataEntrada;
+    private String localCompra;
+    private String tipo;
+    private String marca;
+    private String descricao;
+
+    public int getCodigoItem() {
+        return codigoItem;
+    }
+
+    public void setCodigoItem(int codigoItem) {
+        this.codigoItem = codigoItem;
+    }
+
+    public Date getDataEntrada() {
+        return dataEntrada;
+    }
+
+    public void setDataEntrada(Date dataEntrada) {
+        this.dataEntrada = dataEntrada;
+    }
+
+    public String getLocalCompra() {
+        return localCompra;
+    }
+
+    public void setLocalCompra(String localCompra) {
+        this.localCompra = localCompra;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
